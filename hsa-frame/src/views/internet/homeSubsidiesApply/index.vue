1b18f4efe263131ef233c20d57f4a9b76a827c67
cn32474 <chengjie@powersi.com>
2021-02-18 17:29:53 +0800

    新增人才分类认定及安家补贴申报

--- hsa-frame/src/views/internet/homeSubsidiesApply/index.vue
+++ hsa-frame/src/views/internet/homeSubsidiesApply/index.vue
@@ -1,15 +1,71 @@
 <template>
-  <div>
-    人才分类认定及安家补贴申报
+  <div class="box HomeSubsidiesApply">
+    <box-title title-name="基本信息" />
+    <Basic ref="basic" />
+    <box-title title-name="业务其他信息" />
+    <Business ref="business" />
+    <box-title title-name="高层次人才分类认定信息" />
+    <Cognizance ref="cognizance" />
+    <box-title title-name="业务附件" />
+    <Attachment ref="attachment" />
+    <div class="footer">
+      <my-button type="submit" :loading="save_loading" @click="save" />
+      <my-button type="save" :loading="save_loading" @click="save" />
+      <my-button type="close" />
+    </div>
   </div>
 </template>
 
 <script>
+import BoxTitle from '@/views/components/PageLayers/box-title'
+import Basic from './components/basic'
+import Business from './components/business'
+import Cognizance from './components/cognizance'
+import Attachment from './components/attachment'
+
 export default {
-  name: 'HomeSubsidiesApply'
+  components: {
+    BoxTitle,
+    Basic,
+    Business,
+    Cognizance,
+    Attachment
+  },
+  data() {
+    return {
+      save_loading: false
+
+    }
+  },
+  mounted() {
+    this.resetFields()
+  },
+  methods: {
+    save() {
+      if (!this.$refs.basic.validate()) return false
+      if (!this.$refs.business.validate()) return false
+      if (!this.$refs.cognizance.validate()) return false
+    },
+    resetFields() {
+      this.$refs.basic.resetFields()
+      this.$refs.business.resetFields()
+      this.$refs.cognizance.resetFields()
+    }
+
+  }
 }
 </script>
 
-<style>
+<style lang='scss' scoped>
+.HomeSubsidiesApply {
+  padding-top: 6px;
+  height:auto;
+  padding-bottom: 20px;
+}
+.footer {
+  width: 100%;
+  text-align: right;
+  padding: 10px;
+}
 
 </style>
