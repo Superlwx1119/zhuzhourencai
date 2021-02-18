4abd28990b0f17e95158bc754c7f858aa47f97db
CN32333 <weixiang.liu@powersi.com>
2021-02-18 17:32:03 +0800

    技能人才培训,创新类人才

--- hsa-frame/src/router/modules/internet/index.js
+++ hsa-frame/src/router/modules/internet/index.js
@@ -45,6 +45,55 @@ export default {
       component: () => import('@/views/internet/apartmentApply/index'),
       name: 'ApartmentApply',
       meta: { title: '人才公寓入住申请', icon: 'spot' }
+    },
+    {
+      path: 'skillsTraining',
+      component: () => import('@/views/internet/skillsTraining/skillsSubsidies/index'),
+      name: 'SkillsTraining',
+      meta: { title: '技能人才培训', icon: 'spot' },
+      alwaysShow: true,
+      children: [
+        {
+          path: 'skillsTraining/skillsSubsidies',
+          component: () => import('@/views/internet/skillsTraining/skillsSubsidies/index'),
+          name: 'SkillsSubsidies',
+          meta: { title: '职业技能晋级补贴', icon: '' }
+        }
+      ]
+    },
+    {
+      path: 'leadingTalent',
+      component: () => import('@/views/internet/leadingTalent/index'),
+      name: 'LeadingTalent',
+      redirect: 'noRedirect',
+      meta: { title: '市领军人才申报', icon: 'spot' },
+      alwaysShow: true,
+      children: [
+        {
+          path: 'leadingTalent/innovationTalent',
+          component: () => import('@/views/internet/leadingTalent/innovationTalent/index'),
+          name: 'InnovationTalent',
+          meta: { title: '创新类人才', icon: '' }
+        },
+        {
+          path: 'leadingTalent/innovationTeam',
+          component: () => import('@/views/internet/leadingTalent/innovationTeam/index'),
+          name: 'InnovationTeam',
+          meta: { title: '企业创新团队', icon: '' }
+        },
+        {
+          path: 'leadingTalent/entrepreneurshipTalent',
+          component: () => import('@/views/internet/leadingTalent/entrepreneurshipTalent/index'),
+          name: 'EntrepreneurshipTalent',
+          meta: { title: '创业类人才', icon: '' }
+        },
+        {
+          path: 'leadingTalent/epidemicTalent',
+          component: () => import('@/views/internet/leadingTalent/epidemicTalent/index'),
+          name: 'EpidemicTalent',
+          meta: { title: '疫情防控类人才', icon: '' }
+        }
+      ]
     }
   ]
 }
---
+++ hsa-frame/src/views/internet/leadingTalent/epidemicTalent/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    疫情防控类人才
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EpidemicTalent'
+}
+</script>
+
+<style>
+
+</style>
