e4fb8ef1991601fb7d74042b2f99447b6c701fd1
cn32117 <donghai.huang@powersi.com.cn>
2021-02-19 15:04:01 +0800

    画企业创新团队的页面

---
+++ hsa-frame/src/views/synergyPlatform/evaluationOfMunicipalLeadingTalents/enterpriseInnovationTeam/components/basicInfo.vue
@@ -0,0 +1,60 @@
+<template>
+  <form-dialog
+    class="audit-dialog-wrapper"
+    :title="dialogTitle"
+    :is-show="isDialogVisible"
+    size="big"
+    width="1200px"
+    @update:isShow="isShow"
+  >
+    <div class="box">
+      <div class="box-header">
+        <span class="box-title">团队负责人基本信息</span>
+      </div>
+      <div class="box-body">
+        <el-row :gutter="12">
+          <el-col :span="4" />
+        </el-row>
+      </div>
+    </div>
+    <span slot="footer" class="dialog-footer">
+      <my-button type="close" @click="closeDialog" />
+      <my-button type="save" @click="saveChange" />
+    </span>
+  </form-dialog>
+</template>
+<script>
+export default {
+  model: {
+    prop: 'isDialogVisible',
+    event: 'closeAll'
+  },
+  props: {
+    dialogTitle: {
+      type: String,
+      default: ''
+    },
+    isDialogVisible: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+    isShow(v) {
+      this.$emit('closeAll', v)
+    },
+    closeDialog() {
+      this.$emit('closeAll', false)
+    },
+    saveChange() {}
+  }
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
---
+++ hsa-frame/src/views/synergyPlatform/evaluationOfMunicipalLeadingTalents/enterpriseInnovationTeam/tableConfig.js
@@ -0,0 +1,29 @@
+export const firstColumns = [
+  { label: '企业经营情况', prop: 'x' },
+  { label: 'N-3年', prop: 'xx' },
+  { label: 'N-2年', prop: 'xxx' },
+  { label: 'N-1年', prop: 'xxxx' },
+  { label: 'N年（1月-6月）', prop: 'xxxxx' }
+]
+export const secColumns = [
+  { label: '团队成员', prop: 'x' },
+  { label: '姓名', prop: 'xx' },
+  { label: '出生日期', prop: 'xxx' },
+  { label: '职称/职务', prop: 'xxxx' },
+  { label: '现所在单位', prop: 'xxxxx' },
+  { label: '专业领域', prop: 'xxxxx' },
+  { label: '拟开展工作', prop: 'xxxxx' },
+  { type: 'operation', label: '操作' }
+]
+export const thirdColumns = [
+  { label: '任职机构/期刊/会议等名称', prop: 'x' },
+  { label: '职务', prop: 'xx' },
+  { label: '任期', prop: 'xxx' },
+  { type: 'operation', label: '操作' }
+]
+export const fourColumns = [
+  { label: '项目名称', prop: 'x' },
+  { label: '项目等级', prop: 'xx' },
+  { label: '入选时间', prop: 'xxx' },
+  { type: 'operation', label: '操作' }
+]
